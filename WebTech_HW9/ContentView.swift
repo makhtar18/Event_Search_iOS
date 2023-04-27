96574e6dc1ef76e43bad27a40ad27a20df8239e1
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-04-27 13:31:31 -0700

    Initial Commit

---
+++ WebTech_HW9/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WebTech_HW9
+//
+//  Created by Mehvish Akhtar on 27/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebTech_HW9/WebTech_HW9App.swift
@@ -0,0 +1,17 @@
+//
+//  WebTech_HW9App.swift
+//  WebTech_HW9
+//
+//  Created by Mehvish Akhtar on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WebTech_HW9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
